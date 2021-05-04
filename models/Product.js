1eb4e7b5c5b1c687fad9bfc6d4c7dad462a23d9e
Matthew Goad <matthewxgoad@gmail.com>
2021-05-03 23:20:17 -0500

    updated Product Model

--- models/Product.js
+++ models/Product.js
@@ -9,9 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,3),
+      allowNull: false,
+      unique: true,
+      validate: {},
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+      allowNull: false,
+      validate: {}
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: 'id'
+      },
+      validate: {},
+    },
   },
   {
+    hooks: {},
     sequelize,
     timestamps: false,
     freezeTableName: true,
