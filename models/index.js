205abe42f85432701d229ec12dd5c0b362c291ee
Matthew Goad <matthewxgoad@gmail.com>
2021-05-03 23:24:32 -0500

    updated index Model

--- models/index.js
+++ models/index.js
@@ -5,13 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {foreignKey: 'category_id'});
 // Categories have many Products
-
+Category.hasMany(Product, {foreignKey: 'category_id'});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {through: ProductTag, foreignKey: 'product_id', constraints: false});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(ProductTag, {through: ProductTag, foreignKey: 'tag_id', constraints: false});
 module.exports = {
   Product,
   Category,
